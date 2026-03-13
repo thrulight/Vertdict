d304be72255bd0d0202b1bd2652bb282f7a02990
e.durand.04@gmail.com <e.durand.04@gmail.com>
2026-03-13 16:38:23 +0100

    --home page

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Animation nav bar
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
@@ -7,4 +8,18 @@ var currentScrollPos = window.pageYOffset;
     document.getElementById("navbar").style.top = "-85px";
   }
   prevScrollpos = currentScrollPos;
+}
+
+//Navigation bar mobile version
+function openNav() {
+   document.getElementById("nav-bar-mobile").style.display= "flex";
+   document.getElementById("icon").style.display= "none";
+   document.getElementById("overlay").style.width= "100%";
+   document.getElementById("X").style.display= "flex";
+}
+function closeNav() {
+   document.getElementById("nav-bar-mobile").style.display= "none";
+   document.getElementById("icon").style.display= "flex";
+   document.getElementById("overlay").style.width= "0%";
+   document.getElementById("X").style.display= "none";
 }
